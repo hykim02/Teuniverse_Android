b15a463e0edd28e5a7355cc574d72936af62562d
hykim02 <114382247+hykim02@users.noreply.github.com>
2023-11-29 17:44:27 +0900

    갤러리 구현 중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -39,6 +43,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.14.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.14.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/teuniverse/SignupProfileActivity.kt
+++ app/src/main/java/com/example/teuniverse/SignupProfileActivity.kt
@@ -4,15 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.teuniverse.databinding.ActivityMainBinding
 
 class SignupProfileActivity:AppCompatActivity() {
+
+    //바인딩 객체 선언
+    private var mBinding: ActivitySignupProfileBinding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.signup_profile)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val nextBtn = findViewById<Button>(R.id.next_btn)
         val backBtn = findViewById<ImageButton>(R.id.back_btn)
+        val galleryBtn = findViewById<ImageButton>(R.id.gallery_btn)
+        val profileImg = findViewById<ImageView>(R.id.profile_image)
 
         nextBtn.setOnClickListener {
             val intent = Intent(this, SignupApprovalActivity::class.java)
@@ -26,5 +38,21 @@ class SignupProfileActivity:AppCompatActivity() {
             finish()
         }
 
+        galleryBtn.setOnClickListener{
+            //갤러리 호출
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            activityResult.launch(intent)
+        }
+    }
+
+    private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK && it.data != null){
+            val uri = it.data!!.data
+
+            //화면에 보여주기
+            Glide.with(this).load(uri).into(profileImg)
+        }
     }
 }
