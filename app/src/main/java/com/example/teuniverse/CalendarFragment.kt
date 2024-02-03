214b0a192f2b68c3f765c69f8f68645baff21c9e
hykim02 <hykim4435@naver.com>
2024-02-04 01:09:24 +0900

    오늘 날짜 수정 완료

--- app/src/main/java/com/example/teuniverse/CalendarFragment.kt
+++ app/src/main/java/com/example/teuniverse/CalendarFragment.kt
@@ -2,21 +2,28 @@ package com.example.teuniverse
 
 import android.content.Context
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.text.TextPaint
 import android.text.style.ForegroundColorSpan
 import android.text.style.MetricAffectingSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.example.teuniverse.databinding.FragmentCalendarBinding
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import okhttp3.internal.format
+import java.text.SimpleDateFormat
+import java.time.DayOfWeek
 import java.util.Calendar
+import java.util.Locale
 
 class CalendarFragment : Fragment() {
 
@@ -49,7 +56,7 @@ class CalendarFragment : Fragment() {
     // 달력 커스텀 모음
     private fun customCalendar() {
         // Decorator 추가
-        binding.calendarView.addDecorators(SundayDecorator(), DayDecorator(requireContext()),
+        binding.calendarView.addDecorators(SunDecorator(),
             TodayDecorator(requireContext()))
         // 폰트 사이즈
         binding.calendarView.setDateTextAppearance(R.style.CustomDateTextAppearance)
@@ -67,40 +74,40 @@ class CalendarFragment : Fragment() {
 //        }
     }
 
-
-    // 일요일에 해당하는 날짜 색 변경
-    inner class SundayDecorator : DayViewDecorator {
+    /* 일요일 날짜의 색상을 설정하는 클래스 */
+    private inner class SunDecorator: DayViewDecorator {
         private val calendar = Calendar.getInstance()
         override fun shouldDecorate(day: CalendarDay?): Boolean {
-            day?.copyTo(calendar) // 주어진 캘린더 인스턴스에 오늘의 정보를 복사
-            val weekDay = calendar.get(Calendar.DAY_OF_WEEK) // 일주일을 받아옴
-            return weekDay == Calendar.SUNDAY // 그중 일요일을 리턴
+            day?.copyTo(calendar)
+            val weekDay = calendar[Calendar.DAY_OF_WEEK]
+            return weekDay == Calendar.SUNDAY
         }
         override fun decorate(view: DayViewFacade?) {
-            // 하루(일요일)의 전체 텍스트에 범위의 색 추가
-            view?.addSpan(object : ForegroundColorSpan(Color.RED){})
+            view?.addSpan(ForegroundColorSpan(Color.RED))
         }
     }
 
     /* 선택된 날짜의 background를 설정하는 클래스 */
-    private inner class DayDecorator(context: Context) : DayViewDecorator {
-        private val drawable = ContextCompat.getDrawable(context,R.drawable.calendar_selector)
-        // true를 리턴 시 모든 요일에 내가 설정한 드로어블이 적용된다
-        override fun shouldDecorate(day: CalendarDay): Boolean {
-            return true
-        }
-        // 일자 선택 시 내가 정의한 드로어블이 적용되도록 한다
-        override fun decorate(view: DayViewFacade) {
-            view.setSelectionDrawable(drawable!!)
-        }
-    }
+//    private inner class DayDecorator(context: Context) : DayViewDecorator {
+//        private val drawable = ContextCompat.getDrawable(context,R.drawable.calendar_selector)
+//        // true를 리턴 시 모든 요일에 내가 설정한 드로어블이 적용된다
+//        override fun shouldDecorate(day: CalendarDay): Boolean {
+//            return true
+//        }
+//        // 일자 선택 시 내가 정의한 드로어블이 적용되도록 한다
+//        override fun decorate(view: DayViewFacade) {
+//            view.setSelectionDrawable(drawable!!)
+//        }
+//    }
 
     /* 오늘 날짜의 background를 설정하는 클래스 */
     private class TodayDecorator(context: Context): DayViewDecorator {
         private val drawable = ContextCompat.getDrawable(context,R.drawable.custom_circle_mp)
-        private var date = CalendarDay.today() // 오늘 날짜
+        private var today = CalendarDay.today() // 오늘 날짜(0 = 1월)
+
+        // decorate 여부 결정 함수 (true -> decorate 함수 호출)
         override fun shouldDecorate(day: CalendarDay?): Boolean {
-            return day?.equals(date)!!
+            return day?.equals(today)!!
         }
         override fun decorate(view: DayViewFacade?) {
             view?.setBackgroundDrawable(drawable!!)
