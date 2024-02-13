46af7e45cd779e7065e54c2a38afbb97871c58fa
hykim02 <hykim4435@naver.com>
2024-02-13 22:04:42 +0900

    미디어 구현 완료

--- app/src/main/java/com/example/teuniverse/MediaAdapter.kt
+++ app/src/main/java/com/example/teuniverse/MediaAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.teuniverse
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
@@ -8,7 +10,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 
 class MediaAdapter(private val itemList: ArrayList<MediaContent>): RecyclerView.Adapter<MediaAdapter.MediaViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
@@ -21,6 +22,7 @@ class MediaAdapter(private val itemList: ArrayList<MediaContent>): RecyclerView.
 
         holder.link.text = currentItem.url
         holder.link.visibility = GONE
+
         holder.date.text = currentItem.publishedAt
         holder.title.text = currentItem.title
         holder.viewCount.text = currentItem.views.toString()
@@ -28,6 +30,11 @@ class MediaAdapter(private val itemList: ArrayList<MediaContent>): RecyclerView.
         Glide.with(holder.itemView)
             .load(currentItem.thumbnailUrl)
             .into(holder.thumbnail)
+
+        holder.thumbnail.setOnClickListener {
+            // 썸네일을 클릭했을 때 실행되는 코드
+            onThumbnailClick(holder.itemView, currentItem.url)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -40,5 +47,11 @@ class MediaAdapter(private val itemList: ArrayList<MediaContent>): RecyclerView.
         val viewCount: TextView = itemView.findViewById(R.id.view_count)
         val thumbnail: ImageView = itemView.findViewById(R.id.media_img)
         val link: TextView = itemView.findViewById(R.id.video_link)
+    }
+
+    // 썸네일 클릭 이벤트 처리 메서드
+    private fun onThumbnailClick(itemView: View, url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        itemView.context.startActivity(intent)
     }
 }
