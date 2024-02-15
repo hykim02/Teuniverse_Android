61b02c01112ec26f7c855a03cc4c63259045a329
hykim02 <hykim4435@naver.com>
2024-02-15 13:25:38 +0900

    스케줄 타입 클릭이벤트 완료

--- app/src/main/java/com/example/teuniverse/PopupScheduleType.kt
+++ app/src/main/java/com/example/teuniverse/PopupScheduleType.kt
@@ -2,6 +2,7 @@ package com.example.teuniverse
 
 import android.app.Dialog
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
@@ -22,28 +23,24 @@ class PopupScheduleType(context: Context): Dialog(context) {
         binding.completeBtn.setOnClickListener {
             dismiss()
         }
+
+        binding.closeBtn.setOnClickListener {
+            dismiss()
+        }
     }
 
     private fun setCheckBox() {
-        ScheduleTypeDB.init(context)
-        val typeDB = ScheduleTypeDB.getInstance().all
-        val editor = ScheduleTypeDB.getInstance().edit()
-
         binding.videoChk.setOnClickListener {
-            if (typeDB.containsKey("video")) { // 키가 있는 경우
-                val value = typeDB.getValue("video")
-                Log.d("video", value.toString())
-                if (value == true) {
-                    editor.putBoolean("video", false)
-                    binding.videoChk.setImageResource(R.drawable.checkbox)
-                } else { // false 인 경우
-                    editor.putBoolean("video", true)
-                    binding.videoChk.setImageResource(R.drawable.checkbox_video)
-                }
-            } else { // 키가 없는 경우(처음 클릭)
-                editor.putBoolean("video", true)
-                binding.videoChk.setImageResource(R.drawable.checkbox_video)
-            }
+            confirmCheckBox("video", binding.videoChk)
+        }
+        binding.cakeChk.setOnClickListener {
+            confirmCheckBox("cake", binding.cakeChk)
+        }
+        binding.festivalChk.setOnClickListener {
+            confirmCheckBox("festival", binding.festivalChk)
+        }
+        binding.moreChk.setOnClickListener {
+            confirmCheckBox("more", binding.moreChk)
         }
     }
 
@@ -51,7 +48,9 @@ class PopupScheduleType(context: Context): Dialog(context) {
         ScheduleTypeDB.init(context)
         val typeDB = ScheduleTypeDB.getInstance().all
         val editor = ScheduleTypeDB.getInstance().edit()
-        val src = "R.drawable.checkbox_$chkbox"
+        val src = "checkbox_$chkbox"
+        val resources: Resources = context.resources
+        val packageName: String = context.packageName
 
         if (typeDB.containsKey(chkbox)) { // 키가 있는 경우
             val value = typeDB.getValue(chkbox)
@@ -59,13 +58,18 @@ class PopupScheduleType(context: Context): Dialog(context) {
             if (value == true) {
                 editor.putBoolean(chkbox, false)
                 image.setImageResource(R.drawable.checkbox)
+                editor.apply()
             } else { // false 인 경우
                 editor.putBoolean(chkbox, true)
-                image.setImageResource(src.toInt())
+                val resID = resources.getIdentifier(src, "drawable", packageName)
+                image.setImageResource(resID)
+                editor.apply()
             }
         } else { // 키가 없는 경우(처음 클릭)
             editor.putBoolean(chkbox, true)
-            image.setImageResource(src.toInt())
+            val resID = resources.getIdentifier(src, "drawable", packageName)
+            image.setImageResource(resID)
+            editor.apply()
         }
 
     }
